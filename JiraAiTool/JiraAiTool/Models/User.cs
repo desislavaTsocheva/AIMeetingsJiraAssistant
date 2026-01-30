1a6c42936617077ce333268ef6f3e9a3a24e972f
desislavaTsocheva <desislava_tsocheva@abv.bg>
2026-01-30 16:10:09 +0200

    added css pages for login and qrcode pages

---
+++ JiraAiTool/JiraAiTool/Models/User.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace JiraAiTool.Models
+{
+    public class User
+    {
+        public int Id { get; set; }
+        [Required]
+        public string Email { get; set; } 
+        [Required]
+        public string ApiToken { get; set; } 
+        public string? JiraAccountId { get; set; }
+        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+    }
+}
---
+++ JiraAiTool/JiraAiTool/Services/UserSession.cs
@@ -0,0 +1,9 @@
+﻿namespace JiraAiTool.Services
+{
+    public class UserSession
+    {
+        public string Email { get; set; }
+        public string ApiToken { get; set; }
+        public bool IsAuthenticated => !string.IsNullOrEmpty(ApiToken);
+    }
+}
